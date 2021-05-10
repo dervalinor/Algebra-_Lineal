943c45c6f406bfe7d9a1b25a8649dfe7f85e7a8a
dervalinor <73512305+dervalinor@users.noreply.github.com>
2021-05-10 13:44:36 -0500

    Add files via upload

--- 5_matrix_transpuesta/Matrix_transpuesta.cpp
+++ 5_matrix_transpuesta/Matrix_transpuesta.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 void ask();
-void matrix_transposed(double matrix1[][], int m1, int n1);
-void matrix_simetrica(double matrix1[][], double matrix2[][], int m1, int n1, int m2, int n2);
+void matrix_transposed(double matrix1[100][100], int m1, int n1);//Important give the size of the matrix 
+void matrix_simetrica(double matrix1[100][100], int m1, int n1);
 
 int main(){
   
@@ -17,14 +17,18 @@ int main(){
 }
 
 void ask(){
+
+  double matrix1[100][100]; 
+  int m1, n1, opt;
+
   cout << "Transposed and symmetry matrix" << endl;
   cout << "\n1. Calculate transposed matrix" << endl;
   cout << "2. Calculate symmetry matrix" << endl;  
-  cout << "Choose an option: "; cin >> opt;
+  cout << "\nChoose an option: "; cin >> opt;
   cout << "\n";
   switch(opt){
     case 1: 
-      cout << "Number of rows: "; >> cin m1;
+      cout << "Number of rows: "; cin >> m1;
       cout << "Number of columns: " ; cin >> n1;
       cout << "\nElements of the matrix" << endl;
       cout << "\n"; 
@@ -38,7 +42,6 @@ void ask(){
       break;
 
      case 2: 
-      cout << "Matrix 1" << endl;
       cout << "Number of rows: "; cin >> m1;
       cout << "Number of columns: " ; cin >> n1;
       cout << "Element of the matrix 1: " << endl;
@@ -49,25 +52,42 @@ void ask(){
         }
       }
 
-      cout << "\n";
-      cout << "Matrix 2" << endl;
-      cout << "Number of rows: "; cin >> m1;
-      cout << "Number of columns: " ; cin >> n1;
-      cout << "Element of the matrix 1: " << endl;
-      cout << "\n";
-      for(int i = 0; i < m2; i ++){
-        for(int j = 0; j < n2; j ++){
-          cout << "Element [" << i + 1 << "]" << "[" << j + 1 << "]: " ; cin >> matrix2[i][j];
-        }
+      if( m1 == n1){
+        matrix_simetrica(matrix1, m1, n1);
+      } else {
+        cout << "The matrix is not symmetry" << endl;
       }
-
+     break;
   }
 }
 
-void matrix_transposed(double matrix1[][], int m1, int n1){
-  
+void matrix_transposed(double matrix1[100][100], int m1, int n1){
+  cout << "\nMatrix transposed" << endl;
+  for(int i = 0; i < n1; i ++){
+    for(int j = 0; j < m1; j ++){
+      cout << matrix1[j][i] << " ";
+    }
+    cout << "\n";
+  }
 }
 
-void matrix_simetrica(double matrix1[][], double matrix2[][], int m1, int n1, int m2, int n2){
-  
+void matrix_simetrica(double matrix1[100][100], int m1, int n1){
+  int l = 0, N;
+  cout << "\n";
+  for(int i = 0; i < n1; i ++){
+    for(int j = 0; j < m1; j ++){
+      if(matrix1[i][j] == matrix1[j][i]){
+        l++;
+      }
+    }
+  }
+
+  N = n1*n1;
+
+  if( l == N){
+    cout << "\nMatrix is symmetry" << endl;
+  } else {
+    cout << "Matrix is not symmetry" << endl;
+  }
+
 }

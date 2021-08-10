46a0460b6246b5decc5a846bde1ee3fdd9e1ad1b
dervalinor <dervalar3.14@gmail.com>
2021-08-09 23:44:33 -0500

    Actualizacion determinante

---
+++ 7_Change_rows/change_rows.cpp
@@ -0,0 +1,25 @@
+/* create a program for change the rows of matrix nxn 
+
+save all the elements in vectors*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void ask();
+void change_rows();
+
+int main(){
+  
+  ask();
+  return 0;
+}
+
+void ask(){
+  
+}
+
+void change_rows(){
+
+}

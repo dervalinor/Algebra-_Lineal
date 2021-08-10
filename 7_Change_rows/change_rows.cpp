1e4facdfb4aa7daeeea2b05c04c87cbb5b599bad
dervalinor <dervalar3.14@gmail.com>
2021-08-10 08:28:01 -0500

    Falta programa para cambiar renglones

--- 7_Change_rows/change_rows.cpp
+++ 7_Change_rows/change_rows.cpp
@@ -1,6 +1,4 @@
-/* create a program for change the rows of matrix nxn 
-
-save all the elements in vectors*/
+/* create a program for change the rows of matrix nxn */
 
 #include <iostream>
 #include <math.h>
@@ -8,18 +6,36 @@ save all the elements in vectors*/
 using namespace std;
 
 void ask();
-void change_rows();
+void change_rows(double matrix[100][100], int);
 
 int main(){
-  
   ask();
   return 0;
 }
 
 void ask(){
-  
+  int n, Ri, Rj;
+  double matrix[100][100];
+
+  cout << "Change of row for matrix nxn" << endl;
+  cout << "\n";
+  cout << "Size of the matrix: "; cin >> n;
+  cout << "Number of row to change: "; cin >> Ri;
+  cout << "Number of row for change: "; cin >> Rj;
+  cout << "Elements of the matrix: " << endl;
+  cout << "\n";
+  for(int i = 0; i < n; i ++){
+    for(int j = 0; j < n; j ++){
+      cout << "Element [" << i << "]" << "[" << j << "]: "; cin >> matrix[i][j];
+    }
+  }
+  change_rows(matrix, n, Ri, Rj);
 }
 
-void change_rows(){
-
+void change_rows(double matrix[100][100], int n, int Ri, int Rj){
+  
+  double aux;
+  for(int i = 0; i < n; i ++){
+    
+  }
 }
